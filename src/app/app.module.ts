4aadda91dc0a8a6a007c6cd58d5be2b0679c837c
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-30 12:54:13 +0200

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { ShapesService } from './shapes.service';
+
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent],
+  providers: [ShapesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
